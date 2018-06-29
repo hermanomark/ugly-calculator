1ac9af92aea39649436c6d65a3f4217f98b5253b
unknown <hermano.mark.mh@gmail.com>
2018-06-30 05:57:19 +0800

    handlers done

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-let calculator = {
+const calculator = {
+  result: 0,
+  displayResult() {
+    console.log(this.result)
+  },
   addition(a, b) {
     if (isNaN((a) || (b))) {
       return 'Please input a number';
     } else {
-      return a + b;
+      this.result = a + b;
+      this.displayResult();
     }
   },
   subtraction(a, b) {
     if (isNaN((a) || (b))) {
       return 'Please input a number';
     } else {
-      return a - b;
+      this.result = a - b;
+      this.displayResult();
     }
 
   },
@@ -18,29 +24,47 @@ let calculator = {
     if (isNaN((a) || (b))) {
      return 'Please input a number';
     } else {
-      return a * b;
+      this.result = a * b;
+      this.displayResult();
     }
   },
   division(a, b) {
     if (isNaN((a) || (b))) {
      return 'Please input a number';
     } else {
-      return a / b;
+      this.result = a / b;
+      this.displayResult();
     }
   }
 }
 
-let handlers = {
+const handlers = {
   addition() {
-    
+    num1 = document.getElementById('inputNumber1');
+    num2 = document.getElementById('inputNumber2');
+    calculator.addition(num1.valueAsNumber, num2.valueAsNumber);
+    num1.valueAsNumber = '';
+    num2.valueAsNumber = '';
   },
   subtraction() {
-
+    num1 = document.getElementById('inputNumber1');
+    num2 = document.getElementById('inputNumber2');
+    calculator.subtraction(num1.valueAsNumber, num2.valueAsNumber);
+    num1.valueAsNumber = '';
+    num2.valueAsNumber = '';
   },
   multiplication() {
-
+    num1 = document.getElementById('inputNumber1');
+    num2 = document.getElementById('inputNumber2');
+    calculator.multiplication(num1.valueAsNumber, num2.valueAsNumber);
+    num1.valueAsNumber = '';
+    num2.valueAsNumber = '';
   },
   division() {
-
+    num1 = document.getElementById('inputNumber1');
+    num2 = document.getElementById('inputNumber2');
+    calculator.division(num1.valueAsNumber, num2.valueAsNumber);
+    num1.valueAsNumber = '';
+    num2.valueAsNumber = '';
   }
 }
